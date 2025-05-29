cd2da09c0c596b28cc6c51f53bd994738b9b0e24
Ada438 <ada438438@gmail.com>
2025-05-29 13:38:13 +0300

    HW-2.2

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -2,6 +2,7 @@ package org.skypro.skyshop.basket;
 
 import org.skypro.skyshop.product.Product;
 
+
 public class ProductBasket {
     private final Product[] basket = new Product[5];
     private int size = 0;
@@ -28,11 +29,23 @@ public class ProductBasket {
             System.out.println("В корзине пусто.");
             return;
         }
-        for (int i = 0; i < size; i++) {
-            System.out.println(basket[i].getName() + ": " + basket[i].getPrice());
+
+
+            int specialCount = 0;
+            for (int i = 0; i < size; i++) {
+                Product product = basket[i];
+                System.out.println(product);
+                if (product.isSpecial()) {
+                    specialCount++;
+                }
+            }
+
+            System.out.println("Итого: " + getTotalPrice());
+            System.out.println("Специальных товаров: " + specialCount);
         }
-        System.out.println("Итого: " + getTotalPrice());
-    }
+
+
+
 
     public boolean hasProduct(String name) {
         for (int i = 0; i < size; i++) {
